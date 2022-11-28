22958f2ab4b4b29a49279a6328066abe68bdd1e1
Isaac Mann <imann04@gmail.com>
2022-11-28 21:00:05 +1030

    Clean up generics

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -114,19 +114,19 @@ impl UsersManager {
     UserGet::new(id).send_to(&self.0).await
   }
 
-   /// Retrieve user details. A list of fields to include or exclude may also be specified.
+  /// Retrieve user details. A list of fields to include or exclude may also be specified.
   ///
   /// # Arguments
   /// * `email` - The email of the user to retrieve.
   /// # Scopes
   /// * `read:users`
   /// * `read:user_idp_tokens`
-  pub async fn get_by_email<A, U, S: AsRef<str>>(&self, email: S) -> Result<Vec<User<A, U>>, Auth0Error>
+  pub async fn get_by_email<A, U>(&self, email: &str) -> Result<Vec<User<A, U>>, Auth0Error>
   where
     A: DeserializeOwned + Send + Sync,
     U: DeserializeOwned + Send + Sync,
   {
-    GetUserByEmail::new(email.as_ref()).send_to(&self.0).await
+    GetUserByEmail::new(email).send_to(&self.0).await
   }
 
   /// Retrieve log events for a specific user.
